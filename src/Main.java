16debe6f6797a6b443a0c065d9519db2284eb8aa
Andrey Petrov <Pandand1@yandex.ru>
2022-10-09 15:37:33 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        double bmi = service.calculate(80, 1.8);
+
+        System.out.println("Индекс массы тела:" + bmi);
+    }
+}
